6a0cdfd2af4d0613d1948f43e64885594af456c4
Makoto <48807218+MakotoDesigner@users.noreply.github.com>
2019-07-17 09:19:27 +0200

    create web page

---
+++ tweenmax.js
@@ -0,0 +1,43 @@
+
+TweenMax.from(".arrow", 1, {
+    y: 100,
+    opacity: 0,
+    delay: 0.2,
+ });
+
+ TweenMax.from(".dod", 1, {
+    x: 200,
+    opacity: 0,
+    delay: 0.7,
+ });
+
+ TweenMax.from(".heder", 1, {
+    opacity: 0,
+    delay: 0,
+ });
+
+ TweenMax.from(".btnmenu", 2.5, {
+    x: -100,
+    opacity: 0,
+    delay: 0.5,
+ });
+
+ TweenMax.from(".btnmenu-a", 2, {
+    x: -100,
+    opacity: 0,
+    delay: 0.5,
+ });
+
+ TweenMax.from(".btnmenu-p", 1.5, {
+    x: -100,
+    opacity: 0,
+    delay: 0.5,
+ });
+
+ TweenMax.from(".logo", 1, {
+    y: - 100,
+    opacity: 0,
+    delay: 1,
+ });
+
+ 
